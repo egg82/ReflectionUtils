645ca991b1cf207ebc6b8089a1c353d46ced3f67
egg82 <phantom_zero@ymail.com>
2018-10-29 23:31:09 -0600

    Initial commit

---
+++ src/main/java/ninja/egg82/reflect/PackageFilter.java
@@ -0,0 +1,167 @@
+package ninja.egg82.reflect;
+
+import com.google.common.collect.Sets;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import org.reflections.ReflectionUtils;
+import org.reflections.Reflections;
+import org.reflections.scanners.ResourcesScanner;
+import org.reflections.scanners.SubTypesScanner;
+import org.reflections.scanners.TypeElementsScanner;
+import org.reflections.util.ClasspathHelper;
+import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
+
+public class PackageFilter {
+    public static <T> List<Class<T>> getClasses(Class<T> clazz, String pkg, boolean recursive, boolean keepInterfaces, boolean keepAbstracts, String... excludePackages) {
+        if (clazz == null) {
+            throw new IllegalArgumentException("clazz cannot be null.");
+        }
+        if (pkg == null) {
+            throw new IllegalArgumentException("pkg cannot be null.");
+        }
+
+        Reflections.log = null;
+
+        String excludeString = null;
+        if (excludePackages != null && excludePackages.length > 0) {
+            for (int i = 0; i < excludePackages.length; i++) {
+                excludePackages[i] = "-" + excludePackages[i];
+            }
+            excludeString = String.join(", ", excludePackages);
+        }
+
+        Reflections ref = null;
+        try {
+            ConfigurationBuilder config = new ConfigurationBuilder()
+                    .setScanners(new SubTypesScanner(false),
+                            new ResourcesScanner(),
+                            new TypeElementsScanner())
+                    .setUrls(ClasspathHelper.forPackage(pkg));
+
+            if (excludeString != null) {
+                config = config.filterInputsBy(FilterBuilder.parsePackages(excludeString).include(FilterBuilder.prefix(pkg)));
+            } else {
+                config = config.filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix(pkg)));
+            }
+
+            ref = new Reflections(config);
+        } catch (Exception ex) {
+            return new ArrayList<>();
+        }
+
+        Set<String> typeSet = ref.getStore().get("TypeElementsScanner").keySet();
+        Set<Class<?>> set = Sets.newHashSet(ReflectionUtils.forNames(typeSet, ref.getConfiguration().getClassLoaders()));
+        ArrayList<Class<T>> list = new ArrayList<>();
+
+        for (Class<?> next : set) {
+            if (!keepInterfaces && next.isInterface()) {
+                continue;
+            }
+            if (!keepAbstracts && Modifier.isAbstract(next.getModifiers())) {
+                continue;
+            }
+
+            String n = next.getName();
+            n = n.substring(n.indexOf('.') + 1);
+
+            if (n.contains("$")) {
+                continue;
+            }
+
+            if (!recursive) {
+                String p = next.getName();
+                p = p.substring(0, p.lastIndexOf('.'));
+
+                if (!p.equalsIgnoreCase(pkg)) {
+                    continue;
+                }
+            }
+
+            if (!clazz.equals(next) && !clazz.isAssignableFrom(next)) {
+                continue;
+            }
+
+            list.add((Class<T>) next);
+        }
+
+        return list;
+    }
+
+    public static <T> List<Class<? extends T>> getClassesParameterized(Class<T> clazz, String pkg, boolean recursive, boolean keepInterfaces, boolean keepAbstracts, String... excludePackages) {
+        if (clazz == null) {
+            throw new IllegalArgumentException("clazz cannot be null.");
+        }
+        if (pkg == null) {
+            throw new IllegalArgumentException("pkg cannot be null.");
+        }
+
+        Reflections.log = null;
+
+        String excludeString = null;
+        if (excludePackages != null && excludePackages.length > 0) {
+            for (int i = 0; i < excludePackages.length; i++) {
+                excludePackages[i] = "-" + excludePackages[i];
+            }
+            excludeString = String.join(", ", excludePackages);
+        }
+
+        Reflections ref = null;
+        try {
+            ConfigurationBuilder config = new ConfigurationBuilder()
+                    .setScanners(new SubTypesScanner(false),
+                            new ResourcesScanner(),
+                            new TypeElementsScanner())
+                    .setUrls(ClasspathHelper.forPackage(pkg));
+
+            if (excludeString != null) {
+                config = config.filterInputsBy(FilterBuilder.parsePackages(excludeString).include(FilterBuilder.prefix(pkg)));
+            } else {
+                config = config.filterInputsBy(new FilterBuilder().include(FilterBuilder.prefix(pkg)));
+            }
+
+            ref = new Reflections(config);
+        } catch (Exception ex) {
+            return new ArrayList<>();
+        }
+
+        Set<String> typeSet = ref.getStore().get("TypeElementsScanner").keySet();
+        Set<Class<?>> set = Sets.newHashSet(ReflectionUtils.forNames(typeSet, ref.getConfiguration().getClassLoaders()));
+        ArrayList<Class<? extends T>> list = new ArrayList<>();
+
+        for (Class<?> next : set) {
+            if (!keepInterfaces && next.isInterface()) {
+                continue;
+            }
+            if (!keepAbstracts && Modifier.isAbstract(next.getModifiers())) {
+                continue;
+            }
+
+            String n = next.getName();
+            n = n.substring(n.indexOf('.') + 1);
+
+            if (n.contains("$")) {
+                continue;
+            }
+
+            if (!recursive) {
+                String p = next.getName();
+                p = p.substring(0, p.lastIndexOf('.'));
+
+                if (!p.equalsIgnoreCase(pkg)) {
+                    continue;
+                }
+            }
+
+            if (!clazz.equals(next) && !clazz.isAssignableFrom(next)) {
+                continue;
+            }
+
+            list.add((Class<? extends T>) next);
+        }
+
+        return list;
+    }
+}
